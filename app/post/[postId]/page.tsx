fedc757981fe82ba278db713b492f8893a9b0aaf
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-04-15 23:09:34 +0530

    Fix metadata issue

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -3,23 +3,6 @@ import { getSortedPostsData, getPostData } from "@/lib/posts";
 import Link from "next/link";
 import React from "react";
 
-export function generateStaticParams() {
-  const posts = getSortedPostsData();
-  return posts.map((post) => ({
-    postId: post.id,
-  }));
-}
-
-export function generateMetaData({ params }: { params: { postId: string } }) {
-  const posts = getSortedPostsData();
-  const { postId } = params;
-
-  const post = posts.find((post) => post.id === postId);
-  return {
-    title: post?.title,
-  };
-}
-
 export default async function Post({ params }: { params: { postId: string } }) {
   const posts = getSortedPostsData();
   const { postId } = params;
@@ -31,10 +14,10 @@ export default async function Post({ params }: { params: { postId: string } }) {
   const { title, date, contentHtml } = await getPostData(postId);
   const publishedDate = getFormattedDate(date);
   return (
-    <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+    <main className="px-6 max-w-[800px] prose prose-xl prose-slate dark:prose-invert mx-auto">
       <h1 className="text-3xl mt-4 mb-0">{title}</h1>
       <p className="mt-0">{publishedDate}</p>
-      <article>
+      <article className="mt-10">
         <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
         <p>
           <Link href={"/"}>Back to home</Link>
